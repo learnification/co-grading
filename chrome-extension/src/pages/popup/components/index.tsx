389ad6fcd5ba69b4d4666200be4cd8cdd95bc45d
matt-ham <29mgh29@gmail.com>
2025-06-30 17:09:50 -0700

    remove frontend, restructure

--- chrome-extension/src/pages/popup/components/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Header } from "./Header";
-export { default as CourseNavigator } from "./CourseNavigator";
-export { default as Footer } from "./Footer";
-export { default as Confirmation } from "./Confirmation";

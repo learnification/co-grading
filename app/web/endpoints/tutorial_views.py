e39ea2a71f7750eedbbff6509b2067abc2bbdeb9
matt-ham <29mgh29@gmail.com>
2025-11-06 10:29:46 -0800

    pr fixes

--- app/web/endpoints/tutorial_views.py
+++ app/web/endpoints/tutorial_views.py
@@ -15,6 +15,12 @@ async def create_tutorial_assignment(
 ):
     """
     Creates a Tutorial Assignment and restricts visibility to the Test Student.
+
+    Args: 
+    - courseId: Canvas course ID
+
+    Response:
+    - tutorialId: Assignment ID for newly created tutorial assignment
     """
     try:
         canvas_api = CanvasAPI(
@@ -34,7 +40,6 @@ async def create_tutorial_assignment(
         canvas_api.upload_root(tutorial_data, "tutorial")
 
         return {
-            "courseId": request.courseId,
             "tutorialId": tutorialId,
         }
         
@@ -67,23 +72,22 @@ async def get_tutorial_id(
             domain=x_canvas_base_url,
             course_id=course_id
         )
-        assignment_exists=False
         tutorial_data = canvas_api.get_root_file("tutorial")
 
         if tutorial_data:
             assignment_exists = tutorial_assignment_enabled(canvas_api, tutorial_data['tutorialId'])
-        if assignment_exists:
-                    return tutorial_data
-        else:
-            # Tutorial file exists but assignment is deleted
-            raise HTTPException(
-                status_code=410,
-                detail={
-                    "message": "Tutorial assignment not found",
-                    "tutorialId": tutorial_data['tutorialId'],
-                    "note": "The tutorial file exists but the assignment has been deleted from Canvas"
-                }
-            )
+            if assignment_exists:
+                        return tutorial_data
+            else:
+                # Tutorial file exists but assignment is deleted
+                raise HTTPException(
+                    status_code=410,
+                    detail={
+                        "message": "Tutorial assignment not found",
+                        "tutorialId": tutorial_data['tutorialId'],
+                        "note": "The tutorial file exists but the assignment has been deleted from Canvas"
+                    }
+                )
     except HTTPException as e:
         raise
     except Exception as e:
